53d40ad57354d667158f29e291c9f5d50851ab11
willf <william.walls@my.jcu.edu.au>
2017-08-21 13:17:04 +1000

    warm up prac 004

---
+++ prac_004/warm_up.py
@@ -0,0 +1,21 @@
+numbers = [3, 1, 4, 1, 5, 9, 2]
+
+#numbers = [3, 1, 4, 1, 5, 9, 2]
+#numbers [0]
+#3
+#5 in numbers
+#True
+#"3" in numbers
+#False
+#numbers + [6, 5, 3,]
+#[3, 1, 4, 1, 5, 9, 2, 6, 5, 3]
+#numbers [0] = "ten"
+#numbers
+#['ten', 1, 4, 1, 5, 9, 2]
+#numbers [-1] = 1
+#numbers
+#['ten', 1, 4, 1, 5, 9, 1]
+#numbers [2:]
+#[4, 1, 5, 9, 1]
+#9 in numbers
+#True
